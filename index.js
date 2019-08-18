66599656ac67ccc7fc9ec36ea5c1726c5febbdf3
Will Brown <willbrown@Wills-MacBook-Pro-2.local>
2019-08-18 14:08:00 -0700

    added follow-ons

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const csvtojsonV2 = require('csvtojson')
+const csv                    = require('csvtojson')
+const { getInvestorStats }   = require('./components/Stats')
+const { getTotalShares, addOwnership } = require('./components/Ownership')
+const args                   = process.argv.slice(2)
+const date                   = require('date-and-time') 
+
+const testfile               = args[0]
+const date                   = (args.length > 1 ? args[1] : null)
+
+console.log(testfile, date)
+
+
+csv().fromFile(testfile).then( (json) => {
+
+  let investorStats = getInvestorStats(json)
+  console.log(json)
+  let totalShares = getTotalShares(investorStats)
+  let statsWithOwnership = addOwnership(investorStats, totalShares)
+  console.log(statsWithOwnership)
+
+})
